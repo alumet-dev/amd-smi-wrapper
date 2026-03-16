e2a701497d2731cdcc1a046e42a524c20e831dae
Guillaume Raffin <theelectronwill@gmail.com>
2026-03-16 13:43:32 +0100

    refactor: don't use slice::from_raw_parts when converting a c string to a rust string

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,5 @@
 use bitflags::bitflags;
-use std::{ffi::c_char, slice::from_raw_parts};
+use std::ffi::c_char;
 
 use crate::bindings::*;
 
@@ -153,12 +153,16 @@ pub struct AmdProcess {
     pub evicted_time: u32,
 }
 
-/// Allow to convert ASCII table to usable null terminated string.
+/// Converts a C string to an owned Rust String, with a length limit (the size of `buffer`).
 fn c_buffer_to_string(buffer: &[c_char]) -> String {
+    // cap the length to the size of the buffer
     let length = buffer.iter().position(|&c| c == 0).unwrap_or(buffer.len());
-    let bytes = &buffer[..length];
-    String::from_utf8_lossy(unsafe { from_raw_parts(bytes.as_ptr() as *const u8, bytes.len()) })
-        .into_owned()
+    let chars = &buffer[..length];
+    // convert to bytes
+    // SAFETY: `c_char` is either `i8` or `u8`, and a slice of that can be converted safely to a slice of `u8`s
+    let bytes = unsafe { &*(chars as *const [c_char] as *const [u8]) };
+    // convert to utf-8
+    String::from_utf8_lossy(bytes).into_owned()
 }
 
 impl From<amdsmi_proc_info_t> for AmdProcess {
@@ -206,5 +210,25 @@ impl From<amdsmi_power_info_t> for AmdPowerConsumption {
             mem_voltage: info.mem_voltage,
             power_limit: info.power_limit,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ffi::c_char;
+
+    use super::c_buffer_to_string;
+
+    fn c_array(bytes: &[u8]) -> &[c_char] {
+        unsafe { &*(bytes as *const [u8] as *const [c_char]) }
+    }
+
+    #[test]
+    fn valid_c_buffer_to_string() {
+        let invalid = b"Hello \xF0\x90\x80World\0";
+        assert_eq!(c_buffer_to_string(c_array(b"abc\0")), "abc"); // null-terminated
+        assert_eq!(c_buffer_to_string(c_array(b"abc")), "abc"); // NOT null-terminated
+        assert_eq!(c_buffer_to_string(c_array(invalid)), "Hello �World"); // invalid utf-8 chars
+        assert_eq!(c_buffer_to_string(c_array(b"\0\0\0\0")), ""); // multiple nulls
     }
 }
